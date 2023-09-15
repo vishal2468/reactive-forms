f5533eec0c692ff69e98fbf1cbcae9cedcf72c1c
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-15 16:06:58 +0530

    Conditional validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValdator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password.validator';
@@ -8,24 +8,39 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  registrationForm!: FormGroup;
+  ngOnInit(): void {
+    this.registrationForm=this.fb.group({
+      userName:["Vishal",[Validators.required,Validators.minLength(3),forbiddenNameValdator] ],
+      password:[''],
+      confirmPassword:[''],
+      email:[''],
+      subscribe:true,
+      address:this.fb.group({
+        city:[''],
+        pincode:[''],
+        state:['']
+      })
+    },{validator: PasswordValidator})
+
+    this.registrationForm.get('subscribe')?.valueChanges.subscribe(checkedValue=>{
+      const email=this.registrationForm.get('email');
+      if(checkedValue){
+        email?.setValidators(Validators.required);
+      }
+      else{
+        email?.clearValidators()
+      }
+    })
+  }
 
   constructor(private fb : FormBuilder){}
 
   get userName(){
     return this.registrationForm.get('userName')
   }
-
-  registrationForm=this.fb.group({
-    userName:["Vishal",[Validators.required,Validators.minLength(3),forbiddenNameValdator] ],
-    password:[''],
-    confirmPassword:[''],
-    address:this.fb.group({
-      city:[''],
-      pincode:[''],
-      state:['']
-    })
-  },{validator: PasswordValidator})
 
   loadApiData() {
     this.registrationForm.setValue({

c86ff3d80b0b3c26019cbd2cadaeedd364d965f0
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-15 18:00:28 +0530

    dynamic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValdator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password.validator';
 
@@ -8,49 +8,57 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   registrationForm!: FormGroup;
   ngOnInit(): void {
-    this.registrationForm=this.fb.group({
-      userName:["Vishal",[Validators.required,Validators.minLength(3),forbiddenNameValdator] ],
-      password:[''],
-      confirmPassword:[''],
-      email:[''],
-      subscribe:true,
-      address:this.fb.group({
-        city:[''],
-        pincode:[''],
-        state:['']
-      })
-    },{validator: PasswordValidator})
+    this.registrationForm = this.fb.group({
+      userName: ["Vishal", [Validators.required, Validators.minLength(3), forbiddenNameValdator]],
+      password: [''],
+      confirmPassword: [''],
+      subscribe: true,
+      email: '',
+      address: this.fb.group({
+        city: [''],
+        pincode: [''],
+        state: ['']
+      }),
+      alternateEmails: this.fb.array([]),
+    }, { validator: PasswordValidator })
 
-    this.registrationForm.get('subscribe')?.valueChanges.subscribe(checkedValue=>{
-      const email=this.registrationForm.get('email');
-      if(checkedValue){
+    this.registrationForm.get('subscribe')?.valueChanges.subscribe(checkedValue => {
+      const email = this.registrationForm.get('email');
+      if (checkedValue) {
         email?.setValidators(Validators.required);
       }
-      else{
+      else {
         email?.clearValidators()
       }
     })
   }
 
-  constructor(private fb : FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
-  get userName(){
+  get userName() {
     return this.registrationForm.get('userName')
+  }
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails') as FormArray
+  }
+
+  addAlternateEmails() {
+    this.alternateEmails.push(this.fb.control(''));
   }
 
   loadApiData() {
     this.registrationForm.setValue({
-      "userName":"Vishal",
-      "password":"pass123",
-      "confirmPassword":"pass123",
-      "address":{
-        "city":"RNC",
-        "pincode":"835001",
-        "state":"JH"
+      "userName": "Vishal",
+      "password": "pass123",
+      "confirmPassword": "pass123",
+      "address": {
+        "city": "RNC",
+        "pincode": "835001",
+        "state": "JH"
       }
     })
   }
